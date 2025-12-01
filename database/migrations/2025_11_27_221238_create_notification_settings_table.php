15fe3bc308b32907f2162572da6bdc913858b550
Ammar Sallam <amarslam@gmail.com>
2025-12-01 21:01:41 +0200

    make user_id unique in notification_settings table

--- database/migrations/2025_11_27_221238_create_notification_settings_table.php
+++ database/migrations/2025_11_27_221238_create_notification_settings_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
             $table->id();
 
             $table->foreignId('user_id')
-                  ->constrained('users')
-                  ->cascadeOnDelete();
+                ->unique()
+                ->constrained('users')
+                ->cascadeOnDelete();
 
             $table->boolean('receive_notifications')->default(true);
 
